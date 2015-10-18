8bb36775f8532c4d3af244a832a22d03df61fd55
Wei Wei <shazhouke@gmail.com>
2015-10-18 23:01:03 +0800

    Initiate the project with yeoman

---
+++ test/index.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import xmlSemantic from '../lib';
+
+describe('xml-semantic', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
